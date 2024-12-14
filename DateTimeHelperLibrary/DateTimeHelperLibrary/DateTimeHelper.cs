4e449ca239a626e2aa02856e1311ebc5bf5fc1ef
Selin180796 <christianseleen@gmail.com>
2024-12-14 00:14:16 -0500

    Initial commit with DateTimeHelperLibrary

---
+++ DateTimeHelperLibrary/DateTimeHelperLibrary/DateTimeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DateTimeHelperLibrary
+{
+    public class DateTimeHelper
+    {
+        // Method to add days to a given date
+        public DateTime AddDaysToDate(DateTime date, int days)
+        {
+            return date.AddDays(days);
+        }
+
+        // Method to get current date in a formatted string
+        public string GetCurrentDateFormatted()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        // Method to calculate the difference in days between two dates
+        public int GetDateDifference(DateTime startDate, DateTime endDate)
+        {
+            return (endDate - startDate).Days;
+        }
+    }
+}
